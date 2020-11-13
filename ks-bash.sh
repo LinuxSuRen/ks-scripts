970ab3a2589075680928413460679adafdb6e435
rick <rick@jenkins-zh.cn>
2020-11-13 10:43:21 +0800

    Fix the bash syntax errors

--- ks-bash.sh
+++ ks-bash.sh
@@ -1,5 +1,9 @@
 # Please import this file into your bashrc file
 # source ks-bash.sh
 
-alias ks-devops-disable='kubectl -n kubesphere-system patch cc ks-installer -p '{"spec":{"devops":{"enable":false}}}' --type="merge"'
-alias ks-devops-enable='kubectl -n kubesphere-system patch cc ks-installer -p '{"spec":{"devops":{"enable":true}}}' --type="merge"'
+function ks-devops-disable(){
+	kubectl -n kubesphere-system patch cc ks-installer -p '{"spec":{"devops":{"enable":false}}}' --type="merge"
+}
+function ks-devops-enable(){
+	kubectl -n kubesphere-system patch cc ks-installer -p '{"spec":{"devops":{"enable":true}}}' --type="merge"
+}
