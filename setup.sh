88c8c5283567f8a12a44964aee1cd95299a4c707
Zhao Xiaojie <rick@jenkins-zh.cn>
2020-11-19 09:34:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf ~/.ks-scripts
-git clone https://github.com/LinuxSuRen/ks-scripts/ -o ~/.ks-scripts
+git clone https://github.com/LinuxSuRen/ks-scripts/ ~/.ks-scripts
 echo "source ~/.ks-scripts/ks-bash.sh" >> ~/.bashrc
